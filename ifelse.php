e5ccfec89fa0e13169c994216ae64c3603f7508d
Shivanichahal <shivanichahal0000@gmail.com>
2021-05-17 12:15:46 +0530

    Program to demonstrate ifelse statement

---
+++ ifelse.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>if else</title>
+</head>
+<body>
+	<?php
+	          $num = 12;
+         
+         if ($num%2 == 0)
+            echo "$num is even number"; 
+         
+         else
+            echo "$num is odd number"; 
+?>
+</body>
+</html>
